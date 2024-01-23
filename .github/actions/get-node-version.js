45d81bd6c8a638f08679d9581f0eb37be29fa48f
James Kyburz <james.kyburz@gmail.com>
2024-01-23 13:18:54 +0000

    fix(get-node-version): handle v prefix

--- .github/actions/get-node-version.js
+++ .github/actions/get-node-version.js
@@ -1,5 +1,5 @@
 import { readFile } from 'node:fs/promises'
 export default async function () {
   const env = await readFile('.envrc', 'utf8')
-  return env.match(/declare -r node_version="([^"]+)/)[1]
+  return env.match(/declare -r node_version="v?([^"]+)/)[1]
 }
