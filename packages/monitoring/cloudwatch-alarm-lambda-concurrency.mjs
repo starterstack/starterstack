43242f620f6845e5aa9aa2d5a6d4d247b3c77cc9
James Kyburz <james.kyburz@gmail.com>
2024-01-31 09:45:24 +0000

    fix: update sam-expand property plugin schema

--- packages/monitoring/cloudwatch-alarm-lambda-concurrency.mjs
+++ packages/monitoring/cloudwatch-alarm-lambda-concurrency.mjs
@@ -8,6 +8,7 @@ export const lifecycles = ['pre:expand']
 const THRESHOLD_RATE = 0.8
 const PERIOD = 60
 const EVALUATION_PERIODS = 5
+const DEFAULT_CONCURRENT_EXECUTIONS = 1000
 
 export const lifecycle = async function getRegionLimits({
   command,
@@ -18,12 +19,8 @@ export const lifecycle = async function getRegionLimits({
   if (command === 'build' && argv.includes('--region')) {
     const region = argv[argv.indexOf('--region') + 1]
     const snsAlarmTopic = { Ref: 'SNSAlarmTopic' }
-    const lambda = new LambdaClient({ apiVersion: '2015-03-31', region })
-    const {
-      AccountLimit: {
-        UnreservedConcurrentExecutions: unreservedConcurrentExecutions = 1000
-      } = {}
-    } = await lambda.send(new GetAccountSettingsCommand({}))
+    const unreservedConcurrentExecutions =
+      await getUnreservedConcurrentExecutions(region)
     const threshold = unreservedConcurrentExecutions * THRESHOLD_RATE
     const alarmName = {
       'Fn::Sub': `\${Stack} lambda concurrent executions in ${region} > ${threshold} over the last ${EVALUATION_PERIODS} minutes`
@@ -48,5 +45,25 @@ export const lifecycle = async function getRegionLimits({
         Threshold: threshold
       }
     }
+  }
+}
+
+/**
+ * @param {string} region
+ * @returns {Promise<number>}
+ */
+
+async function getUnreservedConcurrentExecutions(region) {
+  if (process.env.IS_OFFLINE) {
+    return DEFAULT_CONCURRENT_EXECUTIONS
+  } else {
+    const lambda = new LambdaClient({ apiVersion: '2015-03-31', region })
+    const {
+      AccountLimit: {
+        UnreservedConcurrentExecutions:
+          unreservedConcurrentExecutions = DEFAULT_CONCURRENT_EXECUTIONS
+      } = {}
+    } = await lambda.send(new GetAccountSettingsCommand({}))
+    return unreservedConcurrentExecutions
   }
 }
