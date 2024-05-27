aec4b7766b021d3a839799d369e6763921a8a1ed
James Kyburz <james.kyburz@gmail.com>
2024-05-27 11:03:42 +0100

    fix(delete log group script): remove dead code

--- packages/delete-cloudwatch-lambda-log-group.sh
+++ packages/delete-cloudwatch-lambda-log-group.sh
@@ -4,13 +4,7 @@ set -euo pipefail
 
 function delete_lambda_log_group() {
   local stack="${1:?}"
-  local source="${BASH_SOURCE[0]}"
   local region="${2:?}"
-
-  account=$(
-    aws sts get-caller-identity |
-      jq -r '.Account'
-  )
 
   # try and delete ignore all errors (might be ResourceNotFoundException)
   aws logs delete-log-group \
